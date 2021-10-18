b90b9eb9e6d0944d962cb10b826b889215153340
Maximiliano Fonseca <maxfonseca.r@ibm.com>
2021-10-18 17:33:29 -0500

    last commit no solution

--- berlin_clock.js
+++ berlin_clock.js
@@ -56,11 +56,22 @@ function berlin_clock(str) {
     } else {
         // !0 15 30 45 
         // 1 2 3 4 5.....
+        // 0 - 59...
+        if(splitted[1] % 15){
+            // 16, 17, 18 + 
+            console.log('mayor a 15')
+        }else{
+            // YYRYYRYYRYY MINUTES R - 15 MTNS Y - 5
+            // YOOO MINUTE max 5
+            // 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+            min_less_five = splitted[1]/5
+        }
         minRemainder = splitted[1] % 15;
         console.log('YYR'.repeat(minRemainder).length)
         result = result + 'YYR'.repeat(minRemainder) + 'O'.repeat(11 - (3)) + '\n'
     }
     return result;
+    // Refactor cambiar los splitted[] por un minuto = splitted [1], 
     //return "O\nRROO\nRROO\nYYRYYRYYRYY\nYOOO"
 }
 module.exports = berlin_clock;
